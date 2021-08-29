1bffb7bb5cdb0187ac1d9105198e7e17caabcec3
fabgarsan <fabiogarciasanchez@gmail.com>
2021-08-29 16:50:45 -0500

    :file_cabinet: Add new entities to be used
    * Add Application Entity
    * Add Session Entity
    * Add Event Entity

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Session(models.Model):
+    application = models.ForeignKey(Application, related_name='sessions', on_delete=models.PROTECT)
+
+
+class Event(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.PROTECT, related_name='events')
+    category = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
